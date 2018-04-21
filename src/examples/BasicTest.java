c81116b92071070b88975cedc6687a2dcf7f027a
MartinLDeLaTorre <MartinLDeLaTorre@192.168.0.2>
2018-04-20 19:12:58 -0500

    Testing remote push

--- src/examples/BasicTest.java
+++ src/examples/BasicTest.java
@@ -3,8 +3,7 @@ package examples;
 public class BasicTest {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println("Testing remote push!");
 	}
 
 }
